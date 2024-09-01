0d41fefa7b2dc44be7dbe8ce0129e5b7e47cad3a
Tarik J <tarik.jaidani@gmail.com>
2024-09-02 01:31:40 +0200

    Npm Script & faker

--- tests/06Test.spec.ts
+++ tests/06Test.spec.ts
@@ -3,11 +3,13 @@ import {test,expect} from '@playwright/test'
 
 test('dialog Box', async ({page}) =>{
      page.goto('https://practice-automation.com/popups/')
+    
      
      // intercepter le popup
      page.on('dialog', dialog => {
-        expect(dialog.message()).toEqual('Hi there, pal');
+        expect(dialog.message()).toEqual('Hi there, pal!');
         dialog.accept();
      })
+     await page.waitForTimeout(1000)
      await page.getByRole('button' , {name:'Alert Popup'}).first().click()
 })
--- tests/10Test.spec.ts
+++ tests/10Test.spec.ts
@@ -1,6 +1,6 @@
 import {test,expect} from '@playwright/test'
 import { FormPage } from '../page-object/FormPage'
-
+import {faker} from "@faker-js/faker"
 
 test.beforeEach(async({page}) => {
     await page.goto('https://practice-automation.com/form-fields/')
@@ -9,6 +9,9 @@ test.beforeEach(async({page}) => {
 
 test('Value ', async ({page})=> {
     const input = new FormPage(page)
-    await input.inputValue("test", "123456")
+    const randomName = faker.person.fullName()
+    const randomEmail = `${randomName.replace(" ",'')}${faker.number.int(50)}@gmail.com`
+ 
+    await input.inputValue(randomEmail, "123456")
     await input.checkboxes(true)
 })
